eaa2b9697bda8d8d67abc92a445c4fd321dc72c3
tuceacvictor <tuceak16@gmail.com>
2020-07-02 23:53:25 +0300

    created order model

--- app/models/order.model.js
+++ app/models/order.model.js
@@ -4,90 +4,52 @@ module.exports = (sequelize, Sequelize) => {
                 type: Sequelize.INTEGER,
                 primaryKey: true
             },
-            user_begin_id: {
-                type: Sequelize.INTEGER
-            },
-            user_end_id: {
-                type: Sequelize.INTEGER
-            },
-            date_begin: {
-                type: Sequelize.DATE
-            },
-            date_end: {
-                type: Sequelize.DATE
-            },
-            serial: {
-                type: Sequelize.STRING
-            },
-            model_id: {
-                type: Sequelize.INTEGER
-            },
-            description: {
-                type: Sequelize.STRING
-            },
-            master_id: {
-                type: Sequelize.INTEGER
-            },
-            status: {
-                type: Sequelize.INTEGER
-            },
-            cost: {
-                type: Sequelize.DOUBLE
-            },
-            detail_cost: {
-                type: Sequelize.FLOAT
-            },
-            sell: {
-                type: Sequelize.INTEGER
-            },
-            office_id: {
-                type: Sequelize.INTEGER
-            },
-            office_begin_id: {
-                type: Sequelize.INTEGER
-            },
-            office_end_id: {
-                type: Sequelize.INTEGER
-            },
-            replace_status: {
-                type: Sequelize.INTEGER
-            },
-            replace_to: {
-                type: Sequelize.INTEGER
-            },
-            callsms: {
-                type: Sequelize.INTEGER
-            },
             client_id: {
                 type: Sequelize.INTEGER
             },
-            device_type_id: {
+            device_id: {
                 type: Sequelize.INTEGER
             },
-            device_brand_id: {
+            password: {
+                type: Sequelize.STRING
+            },
+            appearance: {
+                type: Sequelize.STRING
+            },
+            equipment: {
+                type: Sequelize.STRING,
+            },
+            problem: {
+                type: Sequelize.STRING,
+            },
+            estimated_price: {
+                type: Sequelize.STRING
+            },
+            note: {
+                type: Sequelize.STRING
+            },
+            ready_date: {
+                type: Sequelize.DATE
+            },
+            prepayment: {
+                type: Sequelize.INTEGER,
+            },
+            manager_id: {
                 type: Sequelize.INTEGER
             },
-            device_name_id: {
+            urgently: {
+                type: Sequelize.BOOLEAN
+            },
+            executor: {
                 type: Sequelize.INTEGER
             },
-            where_type: {
-                type: Sequelize.INTEGER
-            },
-            called: {
-                type: Sequelize.INTEGER
-            },
-            franchise: {
-                type: Sequelize.FLOAT
-            },
-            detail_sell: {
-                type: Sequelize.FLOAT
-            },
-            bank_transfer: {
-                type: Sequelize.FLOAT
+            status: {
+                type: Sequelize.STRING,
+                isIn: ['opened', 'closed']
             }
         },
         {
-            timestamps: false,
+            timestamps: true,
             tableName: 'order'
         });
 };
